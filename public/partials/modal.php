1a1139aa7309d281bea5b2236f1f330e6c154b00
ashdotguru <hello@ash.guru>
2016-07-29 15:56:41 +0100

    include quoting engine modals

--- public/partials/modal.php
+++ public/partials/modal.php
@@ -1,18 +1,27 @@
 <div class="icaal__modal fade" id="quoting-engine-modal" tabindex="-1" role="dialog" aria-labelledby="quoting-engine-modal-label" aria-hidden="true">
   <div class="icaal__modal-dialog" role="document">
     <div class="icaal__modal-content">
-      <div class="icaal__modal-header">
-        <button type="button" class="icaal__close" data-dismiss="icaal__modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <h4 class="icaal__modal-title" id="quoting-engine-modal-label">Modal title</h4>
-      </div>
       <div class="icaal__modal-body">
         ...
       </div>
       <div class="icaal__modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="icaal__modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="icaal__modal fade" id="quoting-engine-redirect-modal" tabindex="-1" role="dialog" aria-labelledby="quoting-engine-redirect-modal-label" aria-hidden="true">
+  <div class="icaal__modal-dialog" role="document">
+    <div class="icaal__modal-content">
+      <div class="icaal__modal-header">
+        <h4 class="icaal__modal-title">Redirecting...</h4>
+      </div>
+      <div class="icaal__modal-body">
+        We are redirecting you to our quoting engine
+      </div>
+      <div class="icaal__modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="icaal__modal">Close</button>
+        <button type="button" class="btn btn-primary" data-toggle="quoting-engine-redirect" data-dismiss="icaal__modal">Continue</button>
       </div>
     </div>
   </div>
