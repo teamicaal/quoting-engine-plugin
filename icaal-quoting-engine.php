df6b0a54ce6a5a4df37e9974810e100c517e52c7
ashdotguru <hello@ash.guru>
2016-07-29 11:56:48 +0100

    add WordPress plugin boilerplate

---
+++ icaal-quoting-engine.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://www.icaal.co.uk
+ * @since             1.0.0
+ * @package           Icaal_Quoting_Engine
+ *
+ * @wordpress-plugin
+ * Plugin Name:       ICAAL Quoting Engine
+ * Plugin URI:        http://www.icaal.co.uk/quoting-engine/
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            ICAAL
+ * Author URI:        http://www.icaal.co.uk
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       icaal-quoting-engine
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-icaal-quoting-engine-activator.php
+ */
+function activate_icaal_quoting_engine() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-icaal-quoting-engine-activator.php';
+	Icaal_Quoting_Engine_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-icaal-quoting-engine-deactivator.php
+ */
+function deactivate_icaal_quoting_engine() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-icaal-quoting-engine-deactivator.php';
+	Icaal_Quoting_Engine_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_icaal_quoting_engine' );
+register_deactivation_hook( __FILE__, 'deactivate_icaal_quoting_engine' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-icaal-quoting-engine.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_icaal_quoting_engine() {
+
+	$plugin = new Icaal_Quoting_Engine();
+	$plugin->run();
+
+}
+run_icaal_quoting_engine();
---
+++ includes/class-icaal-quoting-engine-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.icaal.co.uk
+ * @since      1.0.0
+ *
+ * @package    Icaal_Quoting_Engine
+ * @subpackage Icaal_Quoting_Engine/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Icaal_Quoting_Engine
+ * @subpackage Icaal_Quoting_Engine/includes
+ * @author     ICAAL <ash@icaal.co.uk>
+ */
+class Icaal_Quoting_Engine_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
