d6505170805853413db14df0522100370b87ff2e
ashdotguru <hello@ash.guru>
2016-08-19 11:03:03 +0100

    uncomment to work with old quoting engine

--- public/src/js/modules/event.js
+++ public/src/js/modules/event.js
@@ -1,12 +1,12 @@
 jQuery(function($) {
 
   // iframe height listener
-  // var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-  // var eventer = window[eventMethod];
-  // var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-  // eventer(messageEvent,function(e) {
-  //   document.getElementById("icaal__quote-engine").height = e.data;
-  // },false);
+  var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+  var eventer = window[eventMethod];
+  var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+  eventer(messageEvent,function(e) {
+    document.getElementById("icaal__quote-engine").height = e.data;
+  },false);
 
   function quotingEngineMessage(event) {
     var message = event.data;
